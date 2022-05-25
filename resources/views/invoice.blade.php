699c2bca46025856fd4aac19a3c9d8d76002b6fe
Claudio Fabião <contato@claudiofabiao.com.br>
2022-05-25 14:49:13 -0300

    Corrigido o tipo de conselho na geração do recibo

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -19,7 +19,7 @@
 
     <div style="margin-bottom: 2rem;">
         <h2>{{ $doctor->user->name }}</h2>
-        <p>CRM: {{ $doctor->council_number }}@if ($doctor->cpf) / CPF: {{ number_format(substr($doctor->cpf, 0, 9) . '.' . substr($doctor->cpf, 9, 2), 2, '-', '.') }}@endif</p>
+        <p>{{ $doctor->council_type }}: {{ $doctor->council_number }}@if ($doctor->cpf) / CPF: {{ number_format(substr($doctor->cpf, 0, 9) . '.' . substr($doctor->cpf, 9, 2), 2, '-', '.') }}@endif</p>
     </div>
 
     <p style="text-align: left;line-height: 2;margin-bottom: 2rem;">Recebi do(a) sr(a) {{ $patient->name }}, CPF {{ number_format(substr($patient->document, 0, 9) . '.' . substr($patient->document, 9, 2), 2, '-', '.') }}, a quantia de R$ {{ number_format($amount, 2, ',', '.') }} referente a consulta médica em consultório.</p>
