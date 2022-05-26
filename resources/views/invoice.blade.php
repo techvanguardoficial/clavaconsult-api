655045888496b076333f44afdd6e97687added55
Claudio Fabião <contato@claudiofabiao.com.br>
2022-05-26 17:44:01 -0300

    Corrigido o erro de exibição do CPF na geração do recibo

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -1,5 +1,12 @@
+@php
+    function cpf(string $value): string
+    {
+        return sprintf('%s.%s.%s-%s', substr($value, 0, 3), substr($value, 3, 3), substr($value, 6, 3), substr($value, 9, 2));
+    }
+@endphp
+
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <title>Recibo</title>
@@ -9,27 +16,32 @@
             font-family: sans-serif;
             text-align: center;
         }
+
         h1 {
             text-transform: uppercase;
         }
     </style>
 </head>
 <body>
-    <h1>Recibo</h1>
+<h1>Recibo</h1>
 
-    <div style="margin-bottom: 2rem;">
-        <h2>{{ $doctor->user->name }}</h2>
-        <p>{{ $doctor->council_type }}: {{ $doctor->council_number }}@if ($doctor->cpf) / CPF: {{ number_format(substr($doctor->cpf, 0, 9) . '.' . substr($doctor->cpf, 9, 2), 2, '-', '.') }}@endif</p>
-    </div>
+<div style="margin-bottom: 2rem;">
+    <h2>{{ $doctor->user->name }}</h2>
+    <p>{{ $doctor->council_type }}: {{ $doctor->council_number }} @if ($doctor->cpf)
+            / CPF: {{ cpf($doctor->cpf) }}
+        @endif</p>
+</div>
 
-    <p style="text-align: left;line-height: 2;margin-bottom: 2rem;">Recebi do(a) sr(a) {{ $patient->name }}, CPF {{ number_format(substr($patient->document, 0, 9) . '.' . substr($patient->document, 9, 2), 2, '-', '.') }}, a quantia de R$ {{ number_format($amount, 2, ',', '.') }} referente a consulta médica em consultório.</p>
+<p style="text-align: left;line-height: 2;margin-bottom: 2rem;">Recebi do(a) sr(a) {{ $patient->name }},
+    CPF {{ cpf($patient->document) }}, a
+    quantia de R$ {{ number_format($amount, 2, ',', '.') }} referente a consulta médica em consultório.</p>
 
-    <p style="margin-bottom: 2rem;">{{ $date->format('d/m/Y') }}</p>
+<p style="margin-bottom: 2rem;">{{ $date->format('d/m/Y') }}</p>
 
-    <p>{{ $addressLine1 }}</p>
-    <p>{{ $addressLine2 }}</p>
+<p>{{ $addressLine1 }}</p>
+<p>{{ $addressLine2 }}</p>
 
-    <hr style="width: 60%;margin: 6rem auto 0;">
-    <p style="font-size: .875rem;text-transform: uppercase;text-align: center;">Assinatura</p>
+<hr style="width: 60%;margin: 6rem auto 0;">
+<p style="font-size: .875rem;text-transform: uppercase;text-align: center;">Assinatura</p>
 </body>
 </html>
