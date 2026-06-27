fc0f2f5e7bcf72ed24d88a1a74ea0de0c75873f7
Robson Gomes Pedreira <techvanguardoficial@gmail.com>
2026-06-27 17:42:38 -0300

    importa model

--- app/Http/Controllers/Bot/WhatsappWhiteListController.php
+++ app/Http/Controllers/Bot/WhatsappWhiteListController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Bot;
 
 use App\Http\Controllers\Controller;
+use App\Models\WhatsappWhiteList;
 use Illuminate\Http\Request;
 
 class WhatsappWhiteListController extends Controller
